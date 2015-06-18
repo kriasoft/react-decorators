5c1571b3ff555ae8513da12ca14d7ea3f07d7fa7
Konstantin Tarkus <hello@tarkus.me>
2015-06-18 20:40:12 +0300

    Update unit test

--- test/withViewportSpec.js
+++ test/withViewportSpec.js
@@ -3,15 +3,26 @@
  * Copyright (c) Konstantin Tarkus | MIT License
  */
 
+import React from 'react/addons';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import withViewport from '../src/withViewport.js';
 
+const TestUtils = React.addons.TestUtils;
+
 describe('withViewport', () => {
 
-  it('Should set a viewport', () => {
-    expect(withViewport).to.exist;
-    throw new Error('Not implemented');
+  it('Should set a default viewport', () => {
+    class MyComponent { }
+
+    const ViewportComponent = withViewport(MyComponent);
+    const renderer = TestUtils.createRenderer();
+
+    renderer.render(<ViewportComponent />);
+    const output = renderer.getRenderOutput();
+
+    expect(output._store.props.viewport).to.exist;
+    expect(output._store.props.viewport).to.deep.equal({ width: 1366, height: 768 });
   });
 
 });
